c92861bf8548dcb04586de1455a9a4988304940f
Stephen Bird <sebirdman@gmail.com>
2020-12-21 16:11:34 -0800

    Initial work to send commands to grill

--- traeger.py
+++ traeger.py
@@ -14,12 +14,15 @@ import requests
 import uuid
 import urllib
 import json
+import threading
+import datetime
 
 
 CLIENT_ID = "2fuohjtqv1e63dckp5v84rau0j"
 
+
 class traeger:
-    def __init__(self, username, password, token = None, token_expires = 0, mqtt_url = None, mqtt_url_expires = 0, mqtt_uuid = str(uuid.uuid1())):
+    def __init__(self, username, password, token=None, token_expires=0, mqtt_url=None, mqtt_url_expires=0, mqtt_uuid=str(uuid.uuid1())):
         self.username = username
         self.password = password
         self.token = token
@@ -27,9 +30,9 @@ class traeger:
         self.mqtt_url = mqtt_url
         self.mqtt_url_expires = mqtt_url_expires
         self.mqtt_uuid = mqtt_uuid
-        self.get_grills()
         self.mqtt_client = None
         self.grill_status = {}
+        self.access_token = None
 
     def token_remaining(self):
         return self.token_expires - time.time()
@@ -37,23 +40,57 @@ class traeger:
     def refresh_token(self):
         if self.token_remaining() < 60:
             request_time = time.time()
-            r = requests.post("https://cognito-idp.us-west-2.amazonaws.com/", json = 
-                    {"AuthFlow":"USER_PASSWORD_AUTH",
-                     "AuthParameters":{"USERNAME": self.username,
-                                       "PASSWORD": self.password},
-                     "ClientId": CLIENT_ID},
-                              headers = {'Content-Type': 'application/x-amz-json-1.1',
-                                         'X-Amz-Target': 'AWSCognitoIdentityProviderService.InitiateAuth'})
+            t = datetime.datetime.utcnow()
+            amzdate = t.strftime('%Y%m%dT%H%M%SZ')
+            r = requests.post("https://cognito-idp.us-west-2.amazonaws.com/",
+                              json={
+                                  "ClientMetadata": {},
+                                  "AuthParameters": {
+                                      "PASSWORD": self.password,
+                                      "USERNAME": self.username,
+                                  },
+                                  "AuthFlow": "USER_PASSWORD_AUTH",
+                                  "ClientId": CLIENT_ID
+                              },
+                              headers={'Content-Type': 'application/x-amz-json-1.1',
+                                       'X-Amz-Date': amzdate,
+                                       'X-Amz-Target': 'AWSCognitoIdentityProviderService.InitiateAuth'})
             response = r.json()
             self.token_expires = response["AuthenticationResult"]["ExpiresIn"] + request_time
             self.token = response["AuthenticationResult"]["IdToken"]
+            self.access_token = response["AuthenticationResult"]["AccessToken"]
 
     def get_user_data(self):
         self.refresh_token()
         r = requests.get("https://1ywgyc65d1.execute-api.us-west-2.amazonaws.com/prod/users/self",
-                         headers = {'authorization': self.token})
+                         headers={'authorization': self.token})
         json = r.json()
         return json
+
+    def send_command(self, thingName, command):
+        self.refresh_token()
+        requests.post("https://1ywgyc65d1.execute-api.us-west-2.amazonaws.com/prod/things/{}/commands".format(thingName),
+                      json={
+            'command': command
+        },
+            headers={
+            'Authorization': self.token,
+            "Content-Type": "application/json",
+            "Accept-Language": "en-us",
+            "User-Agent": "Traeger/11 CFNetwork/1209 Darwin/20.2.0",
+        })
+
+    def update_state(self, thingName):
+        self.send_command(thingName, "90")
+
+    def set_temperature(self, thingName, temp):
+        self.send_command(thingName, "117,{}".format(temp))
+
+    def shutdown_grill(self, thingName):
+        self.send_command(thingName, "17")
+
+    def set_timer_sec(self, thingName, time_s):
+        self.send_command(thingName, "12,{}".format(time_s))
 
     def get_grills(self):
         json = self.get_user_data()
@@ -68,47 +105,56 @@ class traeger:
         if self.mqtt_url_remaining() < 60:
             mqtt_request_time = time.time()
             r = requests.post("https://1ywgyc65d1.execute-api.us-west-2.amazonaws.com/prod/mqtt-connections",
-                              headers = {'authorization': self.token})
+                              headers={'Authorization': self.token})
             json = r.json()
-            self.mqtt_url_expires = json["expirationSeconds"] + mqtt_request_time
+            self.mqtt_url_expires = json["expirationSeconds"] + \
+                mqtt_request_time
             self.mqtt_url = json["signedUrl"]
+
+    def _mqtt_connect_func(self):
+        while True:
+            self.mqtt_client.loop_forever()
 
     def get_mqtt_client(self, on_connect, on_message):
         if self.mqtt_client == None:
             self.refresh_mqtt_url()
             mqtt_parts = urllib.parse.urlparse(self.mqtt_url)
-            self.mqtt_client = mqtt.Client(transport = "websockets")
+            self.mqtt_client = mqtt.Client(transport="websockets")
             self.mqtt_client.on_connect = on_connect
             self.mqtt_client.on_message = on_message
             headers = {
                 "Host": "{0:s}".format(mqtt_parts.netloc),
-                }
-            self.mqtt_client.ws_set_options(path="{}?{}".format(mqtt_parts.path, mqtt_parts.query), headers=headers)
+            }
+            self.mqtt_client.ws_set_options(path="{}?{}".format(
+                mqtt_parts.path, mqtt_parts.query), headers=headers)
             context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
             context.check_hostname = False
             context.verify_mode = ssl.CERT_NONE
             self.mqtt_client.tls_set_context(context)
             self.mqtt_client.connect(mqtt_parts.netloc, 443)
-            self.mqtt_client.loop_start()
+            x = threading.Thread(target=self._mqtt_connect_func)
+            x.start()
         return self.mqtt_client
 
     def grill_message(self, client, userdata, message):
         if message.topic.startswith("prod/thing/update/"):
             grill_id = message.topic[len("prod/thing/update/"):]
             self.grill_status[grill_id] = json.loads(message.payload)
-        
+            print(self.grill_status)
+
     def grill_connect(self, client, userdata, flags, rc):
         pass
-    
-    def get_grill_status(self, timeout = 10):
+
+    def subscribe_to_grill_status(self):
         client = self.get_mqtt_client(self.grill_connect, self.grill_message)
         for grill in self.grills:
             if grill["thingName"] in self.grill_status:
                 del self.grill_status[grill["thingName"]]
-            client.subscribe(("prod/thing/update/{}".format(grill["thingName"]),1))
-        for grill in self.grills:
-            remaining = timeout
-            while not grill["thingName"] in self.grill_status and remaining > 0:
-                time.sleep(1)
-                remaining -= 1                    
-        return self.grill_status
+            client.subscribe(
+                ("prod/thing/update/{}".format(grill["thingName"]), 1))
+            self.update_state(grill["thingName"])
+            print("updating state")
+
+    def start(self):
+        self.get_grills()
+        self.subscribe_to_grill_status()
